84c7969c55b7ba919a31b8e887b98c9c41cdfcbe
Brando Paredes <brando_19991@hotmail.com>
2026-03-12 13:54:57 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('solicitudes/{solicitud}/aprobar',  [SolicitudAccesoController::class, 'aprobar'])->name('solicitudes.aprobar');
+    Route::post('solicitudes/{solicitud}/rechazar', [SolicitudAccesoController::class, 'rechazar'])->name('solicitudes.rechazar');
 });
 
 require __DIR__.'/auth.php';
